39ef8c7948a630434e2db7d5b08968725fa1d8f3
Ubuntu <ubuntu@ip-10-32-163-86.ec2.internal>
2013-10-26 20:30:22 +0000

    haha

---
+++ haar_scidb.py
@@ -0,0 +1,7 @@
+import numpy as np
+import math
+from scidbpy import interface, SciDBQueryError, SciDBArray
+sdb = interface.SciDBShimInterface("http://localhost:8080")
+
+def __name__ == "__main__":
+    pass
