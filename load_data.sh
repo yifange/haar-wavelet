8bd25d837dc3d3cad81a16a4e7b1de5f8d8c5244
Fenghuan Lu <lufenghuan@gmail.com>
2013-10-28 15:33:21 +0000

    fix load_data script

--- load_data.sh
+++ load_data.sh
@@ -32,22 +32,41 @@ raw_array_name=${array_name}_raw
 echo $csv_file
 echo $raw_array_name
 
+echo "------------"
+echo "drop array" 
+echo "------------"
+iquery -q "DROP ARRAY $raw_array_name"
+iquery -q "DROP ARRAY $array_name"
+
+echo "------------"
+echo "crate array"
+echo "------------"
 iquery -aq "create array $raw_array_name
 <i:int64,
 j:int64,
 val:double>
-[n=0:*,10,0]"
+[n=0:*,500000,0]"
 
 iquery -aq "create array $array_name
 <val:double>
-[i=0:*,10,0,
-j=0:*,10,0]"
+[i=0:*,500000,0,
+j=0:*,500000,0]"
 
+echo "------------"
+echo "call csv2scidb"
+echo "------------"
 rm -f /tmp/load.scidb
 csv2scidb -i $csv_file -p "NNN" > /tmp/load.scidb &
 sleep 2;
 
-iquery -q "LOAD $raw_array_name FROM '/tmp/load.scidb'"
-iquery -aq "redimension_store($raw_array_name,$array_name)"
+echo "------------"
+echo "Load raw array"
+echo "------------"
+iquery -q "LOAD $raw_array_name FROM '/tmp/load.scidb'" > /dev/null 2>&1
+
+echo "------------"
+echo "redimension_store"
+echo "------------"
+iquery -aq "redimension_store($raw_array_name,$array_name)" > /dev/null 2>&1
 
 
